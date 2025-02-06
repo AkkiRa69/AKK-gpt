0e50ad93eb81d690fa8ec84b12dbdb8c05ccf217
AkkiRa69 <158161839+AkkiRa69@users.noreply.github.com>
2025-02-06 22:54:48 +0700

    final

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,7 +4,7 @@ class GeminiApi {
   GeminiApi(_);
 
   static const apiKey =
-      "AIzaSyCLABIw7tbYdWAgPXfymYMKX00Xbuvg1Rg"; //google gemini
+      "APIKEY"; //google gemini
 
   static Future<String?> generativeText(String userPrompt) async {
     final model = GenerativeModel(
